8fcd0642a5c4a280d62b248965e64d1212e48f03
jpherrmann3 <james_herrmann3@outlook.com>
2024-02-22 18:57:04 -0700

    added maps

---
+++ src/main/java/org/example/sandbox/maps/MapsDemo.java
@@ -0,0 +1,25 @@
+package org.example.sandbox.maps;
+
+import java.util.*;
+
+public class MapsDemo {
+    public static void main(String[] args) {
+        Map<Integer, String> numbers = new HashMap<>();
+
+        numbers.put(1, "One");
+        numbers.put(2, "Two");
+        numbers.put(3, "Three");
+        numbers.put(4, "Four");
+        numbers.put(5, "Five");
+
+        for (Integer i : numbers.keySet()) {
+            System.out.println(i + ": " + numbers.get(i));
+        }
+
+        System.out.println("\n" + "Using lambdas:");
+        numbers.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        System.out.println(numbers);
+
+    }
+}
